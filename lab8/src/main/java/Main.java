be438c5d6d8ea94ff9cfa86f0306392d6342f65c
Mengchao Li <37206605+mengchaoli@users.noreply.github.com>
2018-10-27 20:23:27 -0700

    Add files via upload

--- lab8/src/main/java/Main.java
+++ lab8/src/main/java/Main.java
@@ -18,15 +18,13 @@ class Main
     public static void main(String args[])
     {
         App app = new App() ;
-        //Console c = System.console() ;
-        Scanner sca = new Scanner(System.in);
+        Console c = System.console() ;
         for (;;) {
             System.out.print("\033[H\033[2J") ; // clear the screen
             System.out.flush() ;
             System.out.println( app.display() ) ;
             System.out.print( "Key (Digit or X or Delete) => " ) ;
-            String ch = sca.nextLine();
-            // String ch = c.readLine() ;
+            String ch = c.readLine() ;
             app.key( ch ) ;
         }
     }
